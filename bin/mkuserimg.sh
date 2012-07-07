390b681fad705d30052b57583d51f56277110075
Chris41g <chriswhite@cableone.net>
2012-07-07 00:50:40 -0500

    Modified scripts and binaries and directory layout.
    Modified mkuserimg.sh script to add wiping img to remove padding, also
    added options for gzip and removing journaling. added dummy file
    in system so that directory gets pulled.

--- bin/mkuserimg.sh
+++ bin/mkuserimg.sh
@@ -5,18 +5,28 @@
 function usage() {
 cat<<EOT
 Usage:
-mkuserimg.sh [-s] SRC_DIR OUTPUT_FILE EXT_VARIANT MOUNT_POINT SIZE
+mkuserimg.sh [-sJzw] SRC_DIR OUTPUT_FILE EXT_VARIANT MOUNT_POINT SIZE
 EOT
 }
 
 echo "in mkuserimg.sh PATH=$PATH"
 
-ENABLE_SPARSE_IMAGE=
-if [ "$1" = "-s" ]; then
-  ENABLE_SPARSE_IMAGE="-s"
+ARGUMENTS=
+if [[ "$1" == *s* ]]; then
+  ARGUMENTS="${ARGUMENTS}s"
+fi
+if [[ "$1" == *J* ]]; then
+  ARGUMENTS="${ARGUMENTS}J"
+fi
+if [[ "$1" == *z* ]]; then
+  ARGUMENTS="${ARGUMENTS}z"
+fi
+if [[ "$1" == *w* ]]; then
+  ARGUMENTS="${ARGUMENTS}w"
+fi
+if [[ "$1" == *s* ]] || [[ "$1" == *J* ]] || [[ "$1" == *z* ]] || [[ "$1" == *w* ]]; then
   shift
 fi
-
 if [ $# -ne 4 -a $# -ne 5 ]; then
   usage
   exit 1
@@ -47,7 +57,7 @@ if [ -z $SIZE ]; then
     SIZE=128M
 fi
 
-MAKE_EXT4FS_CMD="make_ext4fs $ENABLE_SPARSE_IMAGE -l $SIZE -a $MOUNT_POINT $OUTPUT_FILE $SRC_DIR"
+MAKE_EXT4FS_CMD="make_ext4fs -$ARGUMENTS -l $SIZE -a $MOUNT_POINT $OUTPUT_FILE $SRC_DIR"
 echo $MAKE_EXT4FS_CMD
 $MAKE_EXT4FS_CMD
 if [ $? -ne 0 ]; then
